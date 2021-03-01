7623225b23ee0f496ef8dc4d8d155966f4676ac9
Arphem <makslav67@gmail.com>
2021-03-01 21:07:27 +0200

    Initial Components Layout

---
+++ src/common/utils/navMenu.tsx
@@ -0,0 +1,22 @@
+import { Icon } from '@material-ui/core';
+import React from 'react';
+import { People, Camera, LocationCity } from '@material-ui/icons';
+
+const menu = [
+    {
+        title: 'Characters',
+        link: '/characters',
+        icon: <People />,
+    },
+    {
+        title: 'Episodes',
+        link: '/episodes',
+        icon: <Camera />,
+    },
+    {
+        title: 'Locations',
+        link: '/locations',
+        icon: <LocationCity />,
+    },
+];
+export default menu;
--- src/features/episodes/index.tsx
+++ src/features/episodes/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import TableHandler from '../../common/ui/Table/table';
 
-function index(props): JSX.Element {
-    return <div></div>;
+function Episodes(props): JSX.Element {
+    return (
+        <div>
+            <TableHandler />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eius necessitatibus
+            voluptatem quae dicta fugiat commodi earum ullam. Quibusdam earum, modi neque placeat
+            qui incidunt dolores veniam nemo ullam, exercitationem in dolore maxime error. Error
+            quasi est amet impedit eos possimus unde maxime eaque voluptate deleniti ipsam laborum,
+            fuga quidem modi quos numquam. In, id vero incidunt doloribus nostrum cum. Aliquam vero
+            eum quibusdam totam at quo labore impedit. Fugit illo unde maxime eius blanditiis
+            officia hic nam possimus, beatae repudiandae perferendis quam commodi totam quis rem
+            harum. Necessitatibus obcaecati architecto at impedit consequatur dolores aliquam
+            tempore omnis totam deserunt harum quidem, ducimus fugit repellendus suscipit in porro
+            commodi assumenda ipsam velit placeat dolore eligendi qui. Ullam ea sequi eaque, vitae
+            odit eveniet consequuntur asperiores blanditiis odio! Repellendus vel error voluptas
+            obcaecati recusandae magni est distinctio reprehenderit, ipsam ab tempora nesciunt
+            nobis. Sequi, magni eaque voluptatum fugiat blanditiis ipsam fugit deserunt cum
+            provident nihil vel iusto placeat nam aut doloribus asperiores ratione? Soluta, dolores
+            perspiciatis! Tempore totam vitae impedit, aspernatur eaque debitis quas autem provident
+            delectus corporis labore hic repellat ex consequatur laboriosam quae blanditiis, maxime
+            et soluta quibusdam libero adipisci? Ipsa provident architecto mollitia explicabo animi
+            quia eum doloremque voluptas tenetur vel sint quae culpa, esse laborum exercitationem
+            voluptate corrupti, incidunt perferendis aliquam fugit placeat delectus? Dicta officia
+            ipsum, illo alias esse a pariatur quas nihil iure provident, ullam quisquam enim!
+            Reprehenderit, nesciunt aliquam quod quibusdam exercitationem, corporis necessitatibus
+            nulla impedit explicabo neque dolore eum reiciendis facere ex tempore.
+        </div>
+    );
 }
 
-export default index;
+export default Episodes;
